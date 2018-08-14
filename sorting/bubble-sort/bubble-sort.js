63c4d490b3ba5ea4cb907219ce1e5847f4b9032d
aofleejay <aofleejay@hotmail.com>
2018-08-14 11:18:02 +0700

    Add bubble sort

---
+++ sorting/bubble-sort/bubble-sort.js
@@ -0,0 +1,17 @@
+const numbers = [5, 9, 3, 4, 2, 8, 1, 7, 0, 6]
+
+const bubbleSort = (array) => {
+  let temp
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        temp = array[j + 1]
+        array[j + 1] = array[j]
+        array[j] = temp
+      }
+    }
+  }
+}
+
+bubbleSort(numbers)
+console.log(`Sorted numbers: ${numbers.join()}.`)

200b26d79bd4cf07b54dcd765904d018b51a041d
aofleejay <aofleejay@hotmail.com>
2018-09-04 14:04:12 +0700

    Change sorting into TypeScript

--- src/algorithms/sorting/bubble-sort.ts
+++ src/algorithms/sorting/bubble-sort.ts
@@ -1,10 +1,10 @@
-const bubbleSort = (array) => {
+const bubbleSort = (array: number[]): void => {
   for (let i = 0, temp; i < array.length; i += 1) {
     for (let j = 0; j < array.length - i; j += 1) {
       if (array[j] > array[j + 1]) {
         temp = array[j + 1]
-        array[j + 1] = array[j] // eslint-disable-line
-        array[j] = temp // eslint-disable-line
+        array[j + 1] = array[j]
+        array[j] = temp
       }
     }
   }
--- src/algorithms/sorting/insertion-sort.ts
+++ src/algorithms/sorting/insertion-sort.ts
@@ -1,10 +1,10 @@
-const insertionSort = (array) => {
+const insertionSort = (array: number[]): void => {
   for (let i = 1, j; i < array.length; i += 1) {
     const key = array[i]
     for (j = i - 1; j >= 0 && key < array[j]; j -= 1) {
-      array[j + 1] = array[j] // eslint-disable-line
+      array[j + 1] = array[j]
     }
-    array[j + 1] = key // eslint-disable-line
+    array[j + 1] = key
   }
 }
 
